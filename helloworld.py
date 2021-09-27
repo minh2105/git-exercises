e4f81f2d1dec4d4c3d9039b73ed6e7fc9fe789fb
minh2105 <minhb1809707@student.ctu.edu.vn>
2021-09-27 16:01:33 +0700

    Modify hellworld.py file

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-print(“Hello world”)
-print(“Hello OSS Development”)
-print(“Hello CTU”)
-=======
 print("Hello world")
 print("Hello OSS Development")
 print("Hello CTU - CICT")
->>>>>>> test_functions
